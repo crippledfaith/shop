dbcd40caec1f6b40ca4be070234bb24130125a23
Taufiq Abdur Rahman <the_wayman@msn.com>
2021-07-15 00:06:12 +0600

    update app.py

--- models/category.py
+++ models/category.py
@@ -5,12 +5,13 @@ class category:
     level=0
     parent_id=""
     
-
-    def __init__(self):
-        self._id = str(uuid.uuid4())
-    
     def __init__(self,dic):
         self._id=dic["_id"]
         self.parent_id=dic["parent_id"]
         self.name=dic["name"]
         self.level=dic["level"]
+        
+    def __init__(self):
+        self._id = str(uuid.uuid4())
+    
+
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from service.product_service import product_service
 from models.category import *  
+
 name = input("name: ")
 level = input("level: ")
 parent_id =input("parent_id")
@@ -7,6 +8,6 @@ parent_id =input("parent_id")
 service = product_service()
 category = category()
 category.name = name
-category.level = level
+category.level = int(level)
 category.parent_id = parent_id
 service.add_category(category)
