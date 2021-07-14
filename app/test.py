30f31e27d6f24860ade190c993b2418ffcc88a55
Taufiq Abdur Rahman <the_wayman@msn.com>
2021-07-14 23:22:31 +0600

    added test.py

---
+++ app/test.py
@@ -0,0 +1,12 @@
+from service.product_service import product_service
+from models.category import *  
+name = input("name: ")
+level = input("level: ")
+parent_id =input("parent_id")
+
+service = product_service()
+category = category()
+category.name = name
+category.level = level
+category.parent_id = parent_id
+service.add_category(category)
