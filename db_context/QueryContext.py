adcc05db8d9f12cb844c91a9fea3422ec031c653
Taufiq Abdur Rahman <the_wayman@msn.com>
2021-07-17 15:21:44 +0600

    GetAll Fixed

--- db_context/QueryContext.py
+++ db_context/QueryContext.py
@@ -9,7 +9,7 @@ class QueryContext(Context):
         return self.db[collection_name].find_one({"_id": item_id})
     
     def get_all(self, collection_name):
-        return self.db[collection_name]
+        return self.db[collection_name].find({})
     
     def get_all_by_condition(self, collection_name, query):
         return self.db[collection_name].find(query)
