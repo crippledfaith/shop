14f777649f98167801c28fabc9c9d1033888ba4b
Taufiq Abdur Rahman <the_wayman@msn.com>
2021-07-18 22:17:41 +0600

    Update CartService

--- service/CartService.py
+++ service/CartService.py
@@ -1,5 +1,7 @@
+from model.Cart import Cart
+from model.Customer import Customer
+from model.Product import Product
 from db_context.QueryContext import QueryContext
-
 
 class CartService:
     
@@ -10,3 +12,13 @@ class CartService:
             cls.__instance.context = QueryContext()
         return cls.__instance
     
+    def create_cart(self,customer):
+        pass
+
+    def add_to_cart(self,cart,product):
+        pass
+
+    def sale_confirm(self,cart):
+        pass
+
+
