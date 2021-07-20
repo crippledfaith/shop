a08fcc755bde6789660d66eedd8ac4a9ff595428
rahul <tauhid.ahmed@gmail.com>
2021-07-21 00:15:25 +0600

    cart service add item, remove item

--- service/CartService.py
+++ service/CartService.py
@@ -3,6 +3,7 @@ from model.Customer import Customer
 from model.Product import Product
 from model.Sale import Sale
 from db_context.QueryContext import QueryContext
+from model.SaleItem import SaleItem
 
 
 class CartService:
@@ -16,23 +17,28 @@ class CartService:
 
     def create_cart(self, customer):
         cart = Cart()
-        cart.sale_id = customer._id
-        sale = self.context.save('sale', customer.__dict__)
+        sale = Sale()
+        cart.sale_id = sale._id
+        sale.customer_id = customer._id
+        sale = self.context.save('sale', sale.__dict__)
         self.context.save('cart', cart.__dict__)
-        return Sale().from_dict(sale)
+        return sale
 
+    def add_item(self, cart, product, quantity, discount):
+        sale_item = SaleItem()
+        sale_item.sale_id = cart.sale_id
+        sale_item.item_id = product._id
+        sale_item.price = product.price
+        sale_item.quantity = quantity
+        sale_item.discount = discount
+        self.context.save('sale_item', sale_item.__dict__)
+        return sale_item
 
-    def add_item(self, cart, product):
-        added = []
-        product_id = Product()
-        if cart._id != '':
-
-        self.context.save('added', product.__dict__)
-
-
-
-    def remove_item(self, cart, product):
-        return self.context.delete('added', product.__dict__)
+    def remove_item(self, sale_item_id):
+        sale_item = SaleItem()
+        sale_item._id = sale_item_id
+        self.context.delete('sale_item', sale_item.__dict__)
+        return None
 
     def sale_confirm(self, cart):
         pass
